37c1d5c360278d21960f776c61a89b6469aa8ce4
avita21 <avita.sharma@tufts.edu>
2017-04-29 16:45:13 -0400

    added http request for the census and some excel datasets

--- test.py
+++ test.py
@@ -2,4 +2,16 @@
 
 # prints out the US Census key
 import os
-print os.environ['USCENSUS_KEY'];
+# import libraries for API requests
+from urllib2 import Request, urlopen, URLError
+
+# make a request from the US Census, EXAMPLE:
+key = os.environ['USCENSUS_KEY'];
+request = Request('http://api.census.gov/data/bds/firms?get=metro,sic1&for=us:*&year2=2012&key='+key);
+
+try:
+	response = urlopen(request)
+	data = response.read()
+	print data
+except URLError, e:
+    print 'Got an error code:', e
